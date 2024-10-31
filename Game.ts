042ee5c26073e41f2e1b145b78195a7280bebbbf
fitia <fitiandrianarisoa@gmail.com>
2024-10-31 09:34:21 +0100

    snacke init

---
+++ Game.ts
@@ -0,0 +1,15 @@
+import { Display } from "./Display.js";
+
+export class Game{
+
+    constructor() {
+    }
+    
+    public getScore():number{
+        return 0;
+    }
+
+    public play(display:Display):boolean{
+        return false;
+    }
+}
